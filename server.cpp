984c2ac8c73aab302c0e43ff044a9521aa8b99c2
Jograj Sidhu <jograjsidhu@Jograjs-MacBook-Pro.local>
2020-12-07 17:39:13 -0800

    adding server class

--- server.cpp
+++ server.cpp
@@ -1,7 +1,59 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <iostream>
+
+
+
 //object to store data received from socket and how many bytes were received
 struct DataBytes{
     int bytes;
     char *data;
+};
+
+//Server class
+class Server{
+    public:
+        Server(void* data);
+        Server(void* data, int port);
+        Server(const Server& orig);
+        virtual ~Server();
+        
+        void shutdown();
+        void init();
+        void loop();
+
+        //callback setters
+        void onConnect(void (*ncc)(Server* server, uint16_t fd, void* data));
+
+    private:
+
+        void* _data;
+
+        //socket file descriptors
+        int _listenFd;
+        int _connFd;
+
+        //client connection data
+        struct sockaddr_storage _clientAddr;
+        
+        //server socket details
+        struct sockaddr_in _servAddr;
+        
+        void (*newConnectionCallback) (Server* server, uint16_t fd, void*);
+
+        //function prototypes
+        void setup(void* data, int port);
+        void initializeSocket();
+        void bindSocket();
+        void startListen();
+        void handleNewConnection();
 };
 
 //A function that reads from the socket, and return both the data and the number of bytes read
@@ -26,3 +78,4 @@ int WriteBytes(uint16_t someSocket, char *data, int x) {
     bytesSent = send(someSocket, data, x, 0);
     return bytesSent;
 }
+
