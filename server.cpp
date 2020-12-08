cf283f34413096c91ee631196433c2c8570a3cf2
Jograj Sidhu <jograjsidhu@Jograjs-MacBook-Pro.local>
2020-12-07 21:57:48 -0800

    added handling and initializing functions

--- server.cpp
+++ server.cpp
@@ -27,16 +27,16 @@ class Server{
         
         void shutdown();
         void init();
-        void loop();
+        //void loop();
 
         //callback setters
-        void onConnect(void (*ncc)(Server* server, uint16_t fd, void* data));
+        //void onConnect(void (*ncc)(Server* server, uint16_t fd, void* data));
 
     private:
 
-        void* _data;
+        void *_data;
 
-        //socket file descriptors
+        //socket file descriptors for the server
         int _listenFd;
         int _connFd;
 
@@ -46,15 +46,69 @@ class Server{
         //server socket details
         struct sockaddr_in _servAddr;
         
-        void (*newConnectionCallback) (Server* server, uint16_t fd, void*);
+        //void (*newConnectionCallback) (Server* server, uint16_t fd, void*);
 
-        //function prototypes
+        //function prototypes for the server
         void setup(void* data, int port);
         void initializeSocket();
         void bindSocket();
         void startListen();
         void handleNewConnection();
 };
+
+void Server::setup(void* data, int port)
+{
+    _data = data;
+	
+	//initialize the server port as the port argument
+	_servAddr.sin_port = port;
+	//create the connection socket for server side
+	_connFd = socket(AF_INET,SOCK_STREAM,0);
+}
+
+
+// Bind the socket to address/port
+void Server::bindSocket()
+{
+	//bind the address to connection socket name
+	bind(_connFd, (struct sockaddr *)&_servAddr, sizeof(_servAddr));
+}
+
+// Listen to incoming connections
+void Server::startListen()
+{
+	//listen at the connection socket
+	listen(_connFd, 1);
+}
+
+// Close the listening socket
+void ::Server::shutdown()
+{
+	//close the socket
+	close(_connFd);
+}
+
+ 
+void Server::handleNewConnection()
+{
+  	std::cout << "[SERVER] [CONNECTION] Waiting for a new connection\n";
+
+	//if socket cant connect, connection fails
+	if ((_listenFd = accept(_connFd, (struct sockaddr *)&_clientAddr,(socklen_t*)&_clientAddr))<0){
+		std::cout<<"[SERVER] [CONNECTION] Connection failed\n";
+	}
+	//otherwise call newConnectionCallback on this server
+	else{
+		newConnectionCallback(this,_connFd, _data);
+	}
+}
+
+void Server::init()
+{
+    initializeSocket();
+    bindSocket();
+    startListen();
+}
 
 //A function that reads from the socket, and return both the data and the number of bytes read
 DataBytes ReadBytes(uint16_t someSocket){
